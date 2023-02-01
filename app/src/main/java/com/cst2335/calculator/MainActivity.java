1762e6044811b103fc654b9220ee2f1e04307ece
RickZhang666 <zhan0775@algonguinlive.com>
2023-02-01 16:10:10 -0500

    first commit complete layout, style, drawable

---
+++ app/src/main/java/com/cst2335/calculator/MainActivity.java
@@ -0,0 +1,43 @@
+package com.cst2335.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private final static String TAG = "CalculatorActivity";
+    private TextView tv_result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+        tv_result = findViewById(R.id.tv_result);
+
+        tv_result.setMovementMethod(new ScrollingMovementMethod());
+
+        findViewById(R.id.btn_cancel).setOnClickListener(this);
+        findViewById(R.id.btn_cancel).setOnClickListener(this);
+        findViewById(R.id.btn_divide).setOnClickListener(this);
+        findViewById(R.id.btn_multiply).setOnClickListener(this);
+        findViewById(R.id.btn_clear).setOnClickListener(this);
+        findViewById(R.id.btn_seven).setOnClickListener(this);
+        findViewById(R.id.btn_eight).setOnClickListener(this);
+        findViewById(R.id.btn_nine).setOnClickListener(this);
+        findViewById(R.id.btn_plus).setOnClickListener(this);
+        findViewById(R.id.btn_four).setOnClickListener(this);
+        findViewById(R.id.btn_five).setOnClickListener(this);
+        findViewById(R.id.btn_six).setOnClickListener(this);
+        findViewById(R.id.btn_minus).setOnClickListener(this);
+        findViewById(R.id.btn_one).setOnClickListener(this);
+        findViewById(R.id.btn_two).setOnClickListener(this);
+        findViewById(R.id.btn_three).setOnClickListener(this);
+        findViewById(R.id.btn_zero).setOnClickListener(this);
+        findViewById(R.id.btn_dot).setOnClickListener(this);
+        findViewById(R.id.btn_equal).setOnClickListener(this);
+        findViewById(R.id.ib_sqrt).setOnClickListener(this);
+    }
+}
